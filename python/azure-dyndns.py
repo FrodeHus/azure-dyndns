09fa40b98f86891be821fb98186f9a7557da8a52
frodehus <frode.hus@outlook.com>
2021-07-22 19:05:05 +0200

    set required arguments and support for environment-based auth

--- python/azure-dyndns.py
+++ python/azure-dyndns.py
@@ -1,15 +1,18 @@
 from azure.mgmt.dns import DnsManagementClient
-from azure.identity import ClientSecretCredential
+from azure.identity import ClientSecretCredential, DefaultAzureCredential
 import argparse
+import os
 
-parser = argparse.ArgumentParser(description="Update Azure DNS record based on current public IP")
-parser.add_argument("--subscription-id", help="Azure subscription ID")
-parser.add_argument("--resource-group", help="Azure resource group name")
-parser.add_argument("--zone", help="Azure DNS zone name")
-parser.add_argument("--record", help="DNS record name to create/update")
-parser.add_argument("--tenant-id", help="Azure tenant ID")
-parser.add_argument("--client-id", help="Azure service principal client id")
-parser.add_argument("--client-secret", help="Service principal client secret")
+parser = argparse.ArgumentParser(
+    description="Update Azure DNS record based on current public IP"
+)
+parser.add_argument("--subscription-id", help="Azure subscription ID", required=True)
+parser.add_argument("--resource-group", help="Azure resource group name", required=True)
+parser.add_argument("--zone", help="Azure DNS zone name", required=True)
+parser.add_argument("--record", help="DNS record name to create/update", required=True)
+parser.add_argument("--tenant-id", help="Azure tenant ID (or set AZURE_TENANT_ID)")
+parser.add_argument("--client-id", help="Azure service principal client id (or set AZURE_CLIENT_ID)")
+parser.add_argument("--client-secret", help="Service principal client secret (or set AZURE_CLIENT_SECRET)")
 args = parser.parse_args()
 
 subscription_id = args.subscription_id
@@ -17,9 +20,10 @@ tenant_id = args.tenant_id
 client_id = args.client_id
 client_secret = args.client_secret
 
-credentials = ClientSecretCredential(
-    tenant_id, client_id, client_secret
-)
+if os.getenv("AZURE_TENANT_ID") and os.getenv("AZURE_CLIENT_ID") and os.getenv("AZURE_CLIENT_SECRET"):
+    credentials = DefaultAzureCredential()
+else:
+    credentials = ClientSecretCredential(tenant_id, client_id, client_secret)
 
 resource_group = args.resource_group
 dns_zone = args.zone
@@ -29,9 +33,7 @@ record = args.record
 def update_dns(ip: str):
     import json
 
-    dns_client = DnsManagementClient(
-        credentials, subscription_id=subscription_id
-    )
+    dns_client = DnsManagementClient(credentials, subscription_id=subscription_id)
     record_set = dns_client.record_sets.create_or_update(
         resource_group,
         dns_zone,
